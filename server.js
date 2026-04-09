d41bccac649b2bfe740dd5512d22c9b154f7be9d
Luciana Noelia Da Silva <luzzdevictoria@gmail.com>
2026-04-09 17:37:04 -0300

    Fix CORS: permitir peticiones desde cualquier origen

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(helmet());
 
 // Habilitar CORS para desarrollo (permite peticiones desde frontend)
 app.use(cors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:3000', 'http://localhost:8080', 'http://127.0.0.1:8080', 'http://127.0.0.1:58125'],
+    origin: '*',
     credentials: true
 }));
 
